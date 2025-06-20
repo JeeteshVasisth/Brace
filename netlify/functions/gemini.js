c217ab1f026e9fa6299de353d2076ff29d8d0b8d
JeeteshVasisth <jeeteshvasisth@gmail.com>
2025-06-20 13:57:33 +0530

    Update gemini.js

--- netlify/functions/gemini.js
+++ netlify/functions/gemini.js
@@ -13,7 +13,7 @@ export async function handler(event, context) {
   }
 
   const geminiResponse = await fetch(
-    `https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=${API_KEY}`,
+    `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${API_KEY}`,
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
