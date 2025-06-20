4f9c83a0f1ad39c8a3d34bbc6e5f436af36553ad
JeeteshVasisth <jeeteshvasisth@gmail.com>
2025-06-20 14:30:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const closeMenuBtn = document.querySelector("#close-menu-btn");
 
 menuBtn.addEventListener("click", () => {
   header.classList.toggle("show-mobile-menu");
-  header.classList.togglt("hide-navbar");
+  header.classList.toggle("hide-navbar");
 })
 
 closeMenuBtn.addEventListener("click", () => {
