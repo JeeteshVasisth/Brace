295e84a15c5a2837f6c4a56c4f703109f962d355
JeeteshVasisth <jeeteshvasisth@gmail.com>
2025-06-20 21:27:21 +0530

    Update disasterNews.js

--- netlify/functions/disasterNews.js
+++ netlify/functions/disasterNews.js
@@ -11,8 +11,7 @@ export async function handler(event, context) {
   }
    const url = new URL("https://newsapi.org/v2/top-headlines");
     url.searchParams.append("q", keyword);
-    url.searchParams.append("category", category);
-    url.searchParams.append("country", country);
+    url.searchParams.append("category", "Science AND General");
     url.searchParams.append("language", "en");
     url.searchParams.append("pageSize", "10");
   //const url = `https://newsapi.org/v2/top-headlinescategory=scienceq=${keyword}&language=en&sortBy=publishedAt&pageSize=5`;
